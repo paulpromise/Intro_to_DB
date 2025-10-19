7613ed5b1ceb956bd50e428ed12c7a3d1589ed92
Paul Promise <paul.promise@outlook.com>
2025-10-19 10:16:09 +0000

    created task 4 to show all table in alx store book

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Script: task_4.sql
+-- This script prints the full description of the table books 
+-- from the database passed as an argument (alx_book_store)
+
+USE alx_book_store;
+
+SHOW CREATE TABLE books;
