84448ba26c89d4752f2c0492e623d446dc1b9142
Paul Promise <paul.promise@outlook.com>
2025-10-19 10:16:50 +0000

    created task 4 to show all table in alx store book

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,14 @@
 -- Script: task_4.sql
--- This script prints the full description of the table books 
--- from the database passed as an argument (alx_book_store)
+-- This script prints the full description of the table books
+-- from the database alx_book_store without using DESCRIBE or EXPLAIN
 
-USE alx_book_store;
-
-SHOW CREATE TABLE books;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
