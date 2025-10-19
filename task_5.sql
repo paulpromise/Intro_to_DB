dca380a0d8babf26f908136bd0d10427b5cedc0f
Paul Promise <paul.promise@outlook.com>
2025-10-19 10:25:45 +0000

    corrected the table name from upercase to lowercase

--- task_5.sql
+++ task_5.sql
@@ -4,5 +4,5 @@
 -- DATABASE: alx_book_store
 -- ===========================================================
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
