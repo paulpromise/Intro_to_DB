f56feacbbc31aa8eda18524bb08482f2a9aec7e1
Paul Promise <paul.promise@outlook.com>
2025-10-19 10:24:02 +0000

    creatd task 5 to insect data into Customers table - one roll data

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- ===========================================================
+-- SCRIPT: task_5.sql
+-- PURPOSE: Insert a single row into the Customers table
+-- DATABASE: alx_book_store
+-- ===========================================================
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
